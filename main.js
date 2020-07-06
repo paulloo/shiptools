00806215b8d00709780637a57ec7e3d0694a0478
paul <dongpoding@gmail.com>
2020-07-06 17:15:53 +0800

    add function

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import {version} from './package.json'
 
 import * as num from './libs/number'
+import * as fn from './libs/function'
 import * as url from './libs/url'
 
 const shiptools = {
     ...num,
     ...url,
+    ...fn,
     version
 }
 
