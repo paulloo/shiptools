b8c0c7c3d1df2967cae3c41f46e7c6bbad7a66bb
paul <dongpoding@gmail.com>
2020-07-24 15:11:03 +0800

    string

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ import {version} from './package.json'
 import * as num from './libs/number'
 import * as fn from './libs/function'
 import * as url from './libs/url'
+import * as string from './libs/string'
 
 const shiptools = {
     ...num,
     ...url,
     ...fn,
+    ...string,
     version
 }
 
