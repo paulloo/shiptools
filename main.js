8175007ce54356ecccdc0d158760e4fd09c69916
paul <dongpoding@gmail.com>
2020-11-25 09:40:12 +0800

    types

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ import * as num from './libs/number'
 import * as fn from './libs/function'
 import * as url from './libs/url'
 import * as string from './libs/string'
+import * as types from './libs/types'
 
 const shiptools = {
     ...num,
     ...url,
     ...fn,
     ...string,
+    ...types,
     version
 }
 
