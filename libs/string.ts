2460c8febc1cb21bc9be066c9ad7fba75ebc59a8
paul <dongpoding@gmail.com>
2021-04-12 10:02:49 +0800

    move taransparam to string bolck

--- libs/string.ts
+++ libs/string.ts
@@ -46,3 +46,22 @@ export function uuid(len = 8, radix = 16): string {
   
     return value.join('')
 }
+
+
+/**
+ * 转换JSON格式字符串为url拼接字符串
+ * @param param JSON形式的参数字符串
+ */
+ export const transParams = (param) => {
+  console.log('enter transparam', param);
+  let paramObj = JSON.parse(param);
+  let paramStr = ''
+  for (const key in paramObj) {
+    if (paramObj.hasOwnProperty(key)) {
+      const element = paramObj[key];
+      paramStr = `${paramStr}${paramStr.length>0?'&':'?'}${key}=${element}`;
+    }
+  }
+  console.log('transParams转换后的参数', paramStr);
+  return paramStr;
+}
